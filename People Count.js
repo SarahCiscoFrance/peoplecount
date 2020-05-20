2058b28fb656e66de7fe79a77571f6313ecfc12c
SarahCiscoFrance <sarah@ciscofrance.com>
2020-05-20 20:51:59 +0200

    add digital signage feature

--- People Count.js
+++ People Count.js
@@ -3,8 +3,12 @@
 //
 // v0.1       Info: Count maximum people, show notification when >max nr of people in the room
 // v0.2       Changed text display method
-
+// v0.3       (rudferna@cisco.com) Digital signage
 const xapi = require('xapi');
+xapi.config.set("Standby Signage Mode", "On");
+xapi.config.set("WebEngine Mode", "On");
+
+
 const maxPeople = 5
 const alertDuration = 30
 console.log('MACRO STARTED - COVID-19 People Count');
@@ -22,7 +26,12 @@ function postStatusCall(amount) {
    console.log('DEBUG - Detected: ' + amount + ', max: ' + maxPeople);
    if (amount > maxPeople) {
        console.log('DEBUG - Alerting');
-       displayTextOnScreen("Too many people detected                       Room capacity = " + maxPeople)
+       displayTextOnScreen("Too many people detected                       Room capacity = " + maxPeople);
+       setTimeout(function(){ 
+         xapi.config.set("Standby Signage Url", "https://www.wired.com/story/elegant-mathematics-social-distancing/");
+        xapi.command('Standby Halfwake'); 
+       }, 3000);
+       
    };
 }
 
