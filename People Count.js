d2ebe02958a3b485d456ddfadce435e8070f7b84
SarahCiscoFrance <sarah@ciscofrance.com>
2020-05-22 19:18:40 +0200

    add OPTION Digital Signage

--- People Count.js
+++ People Count.js
@@ -5,6 +5,8 @@
 // v0.2       Changed text display method
 // v0.3       (rudferna@cisco.com) Digital signage
 const xapi = require('xapi');
+const OPTION = 1; // 1: For DIGITAL SIGNAGE OR 2: For ALERT MESSAGE  <== CHANGE HERE OPTIONS
+const url = "https://www.wired.com/story/elegant-mathematics-social-distancing/"; // url of the web page that will be displayed
 xapi.config.set("Standby Signage Mode", "On");
 xapi.config.set("WebEngine Mode", "On");
 
@@ -27,11 +29,12 @@ function postStatusCall(amount) {
    if (amount > maxPeople) {
        console.log('DEBUG - Alerting');
        displayTextOnScreen("Too many people detected                       Room capacity = " + maxPeople);
-       setTimeout(function(){ 
-         xapi.config.set("Standby Signage Url", "https://www.wired.com/story/elegant-mathematics-social-distancing/");
-        xapi.command('Standby Halfwake'); 
-       }, 3000);
-       
+       if(OPTION === 1){
+          setTimeout(function(){ 
+                  xapi.config.set("Standby Signage Url", url);
+                  xapi.command('Standby Halfwake'); 
+          }, 4000);
+       }
    };
 }
 
